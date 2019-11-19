c2bb2de9c8033b1b5911a967895772f3450e1194
0628dolphin <dhotmannat1@gmail.com>
2019-11-19 17:39:40 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Mannat Dhot, Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Dhot_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Dhot_Projects.html">Projects.html</a></li></br>
+      <li><a href="Dhot_Resume.html">Resume.html</a></li></br>
+      <li><a href="Dhot_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
